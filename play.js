2b209197ed600baa4c8faae7178056c84f395107
Lisa Walters <lisacampiwalters678@gmail.com>
2022-08-06 21:33:08 -0600

    completed tasks, and then experiemented with other problems

--- play.js
+++ play.js
@@ -1,16 +1,28 @@
-let list = [
-    {color: 'green',
-      size: 1
-    },
-    {color: 'pink',
-     size: 8
-    },
-    {color: 'black',
-     size: 5
-    },
-    {color: 'orange',
-     size: 100    
-}
+// Sum Numbers
+// function sum(numbers) {
+//   "use strict";
+//     if(numbers === [' ']){
+//       console.log(0);
+//     } else {
+//       let total = 0;
+//       for(let i = 0; i < numbers.length; i ++){
+//         total = total + numbers[i];
+//       }
+//       console.log(total);
+//     };
+// };
 
-];
-console.log(list.length);
+// sum([ ]);
+
+// function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+//   let rna = dna.replace(/T/g, 'U');
+//   console.log(rna);
+// }
+
+// DNAtoRNA('GCATGATT');
+
+function opposite(num){
+  return num > 0? -num: num;
+}
+opposite(4);
